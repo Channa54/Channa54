932a5b8785ab05cc122c907a2036d48cb5663f83
channu <channabasava0190nagwar@gmail.com>
2024-02-27 14:58:16 +0530

    first commit

---
+++ src/test/java/practice/A2.java
@@ -0,0 +1,23 @@
+package practice;
+
+public class A2 {
+
+	public static void main(String[] args) {
+
+   String a = "channu";
+   String b =" ";
+   char ch;
+   
+   for (int i =0;i<a.length();i++) {
+	   
+	      ch = a.charAt(i);
+	      
+	  b= ch+b;    
+   }
+		
+		System.out.println(b);
+		
+		
+	}
+
+}
